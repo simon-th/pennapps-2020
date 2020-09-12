7a1d6bcd10598222cc5a267a3eab3c940edf0245
delpinolisette <delpinolisette@gmail.com>
2020-09-12 10:52:59 -0400

    scikit image

---
+++ im_analysis.py
@@ -0,0 +1,30 @@
+# https://pillow.readthedocs.io/en/stable/handbook/tutorial.html#more-on-reading-images
+"""
+import urllib.request
+import PIL
+from PIL import Image
+#breakpoint()
+
+# get image
+#image_url='https://imgur.com/a/KcK0oKQ.png'
+image_url='https://imgur.com/a/7wAlncp'
+
+urllib.request.urlretrieve(image_url, "im.jpg")
+#breakpoint()
+img = PIL.Image.open("im.jpg")
+# show im
+img.show()
+"""
+# clustering 
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+# comp vision
+import cv2
+from collections import Counter
+from skimage.color import rgb2lab, deltaE_cie76
+import os
+
+#%matplotlib inline # for ipython
+image = cv2.imread('sam.jpg')
+breakpoint()
